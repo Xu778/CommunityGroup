5d82aea0b72c31c0d70d428722783b799fcdb789
Xu778 <416996370@qq.com>
2021-09-09 09:58:50 +0800

    first

---
+++ app/Api/controller/Index.php
@@ -0,0 +1,12 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\Api\controller;
+
+class Index
+{
+    public function index()
+    {
+        return '您好！这是一个[Api]示例应用';
+    }
+}
---
+++ app/common.php
@@ -0,0 +1,5 @@
+<?php
+// 应用公共文件
+function testTest(){
+    return 123;
+}
